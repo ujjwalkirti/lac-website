a427c71df23440ef19e124e250ea250d71ea9887
Darshan <darshanbpatel1904@gmail.com>
2023-04-11 17:22:43 +0530

    added random data in blogs.ts

--- src/local-data/Blogs.ts
+++ src/local-data/Blogs.ts
@@ -1,1 +1,11 @@
-export const blogs: Blog[] = [];
+export const blogs: Blog[] = [
+    {
+        name: "blog1",
+        author: "Me",
+        designation: "",
+        about: "first blog",
+        content: "This is first blog",
+        illustration: "",
+        date: 19,
+    }
+];
