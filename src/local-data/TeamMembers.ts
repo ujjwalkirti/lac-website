634bccdd3619c050074538f783d177c3fd34769c
Geetanjali Ghatak <geetanjalighatak@gmail.com>
2023-04-11 17:41:38 +0530

    data added

--- src/local-data/TeamMembers.ts
+++ src/local-data/TeamMembers.ts
@@ -1,1 +1,32 @@
-export const team_member: Team_Member[] =[];
+export const team_member: Team_Member[] =[
+    {
+        picture:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        name:"Priyanshi Shah",
+        post:"Junior Secretary",
+        desc:"Lorem ipsum"
+    },
+    {
+        picture:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        name:"Priyanshi Shah",
+        post:"Junior Secretary",
+        desc:"Lorem ipsum"
+    },
+    {
+        picture:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        name:"Priyanshi Shah",
+        post:"Junior Secretary",
+        desc:"Lorem ipsum"
+    },
+    {
+        picture:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        name:"Priyanshi Shah",
+        post:"Junior Secretary",
+        desc:"Lorem ipsum"
+    },
+    {
+        picture:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        name:"Priyanshi Shah",
+        post:"Junior Secretary",
+        desc:"Lorem ipsum"
+    },
+];
