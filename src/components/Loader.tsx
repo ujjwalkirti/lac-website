7f4d2bef0e3740866c816e009a315ec3b613dc55
Ujjwal Kirti <64329707+ujjwalkirti@users.noreply.github.com>
2023-04-14 23:25:34 +0530

    Resolved page transition loader issue

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Loader = () => {
   const { theme } = useTheme();
   return (
-    <div className="fixed z-50 h-screen w-screen flex justify-center items-center">
+    <div className="fixed z-50 h-screen w-screen flex justify-center items-center bg-white">
       {theme === "dark" ? (
         <Image src={`/logo-black.jpeg`} height={60} width={60} alt="LAC logo" className="rounded-full"/>
       ) : (
