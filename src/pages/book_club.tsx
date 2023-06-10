393b5c3e524b1addefdb3252b1265f2b7a274a11
Darshan <darshanbpatel1904@gmail.com>
2023-06-10 16:34:03 +0530

    Minor changes #31

--- src/pages/book_club.tsx
+++ src/pages/book_club.tsx
@@ -5,6 +5,7 @@ import { db2 } from "@/Firebase";
 import { libre_caslon_text } from "@/utils";
 import Head from "next/head";
 import BookDisplayBox from "@/components/Book Club/BookDisplayBox";
+import { SiTarget } from "react-icons/si";
 
 type props = {
   books: Book[];
@@ -40,6 +41,20 @@ const BookClub = ({ books }: props) => {
         ook <FirstLetterCapital letter="C" bgColor="#DA8E63" />
         lub
       </p>
+      <p className="text-center  leading-[30px] lg:leading-[40px]">  
+        Do you often get lost in the world of dreams setting off on adventures
+        with Harry, Percy or Katniss? <br />
+        Do you sometimes imagine yourself living in the enchanted land of
+        Westeros or solving mysteries with Hercule Poirot?
+        <br /> Do you feel mesmerized by Shakespeare's sonnets? <br />
+        Or are you still new to this fantastic world of literature? <br />
+        Well, don't you worry! We've got you covered. LAC has its very own
+        bookclub for all passionate readers out there. <br />
+      </p>
+      <div className="my-10 flex  justify-center items-center lg:w-1/3 mx-auto border dark:border-white border-[#2C1810] px-3 py-2 gap-2 rounded-md">
+        Venue: 3rd floor, Central Library 
+        <SiTarget className="" />
+      </div>
       
       <div className="lg:grid lg:grid-cols-2 gap-4">
         {currentBooks.map((book: Book, index: number) => {
